862afbcad3478c44c09bdc35f300d7ddfa196344
Astitwa2424 <darsanme49@gmail.com>
2025-01-14 19:52:28 +1100

    addedsecretkey

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 import { withPayload } from '@payloadcms/next/withPayload'
-
 import redirects from './redirects.js'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
@@ -12,7 +11,6 @@ const nextConfig = {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
         const url = new URL(item)
-
         return {
           hostname: url.hostname,
           protocol: url.protocol.replace(':', ''),
@@ -22,6 +20,10 @@ const nextConfig = {
   },
   reactStrictMode: true,
   redirects,
+  env: {
+    PAYLOAD_SECRET: process.env.PAYLOAD_SECRET,
+    NEXT_PUBLIC_SERVER_URL: NEXT_PUBLIC_SERVER_URL,
+  },
 }
 
 export default withPayload(nextConfig)
